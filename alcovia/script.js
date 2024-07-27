41467fb8e9cb457574da2060b6cd6723a4f04e99
Sanskar-001 <sanskarmishra1122@gmail.com>
2024-07-27 15:43:05 +0530

    fixed bugs and improved the testimonial and collaboration section

--- alcovia/script.js
+++ alcovia/script.js
@@ -1,7 +1,65 @@
 $(document).ready(function(){
-    $(window).on('scroll',function(){
-        var scroll = $(window).scrollTop(); 
-        console.log(scroll);
-    })
+    // $(window).on('scroll',function(){
+    //     var scroll = $(window).scrollTop(); 
+    //     console.log(scroll);
+    // })
+
+    // counter for about us
+    const counters = document.querySelectorAll('.count');
+    const speed = 200;
+    
+    counters.forEach((counter) => {
+      const updateCount = () => {
+        const target = parseInt(counter.getAttribute('data-target'));
+        const count = parseInt(counter.innerText);
+        const increment = Math.trunc(target / speed);
+    
+        if (count < target) {
+          counter.innerText = count + increment;
+          setTimeout(updateCount, 1);
+        } else {
+          counter.innerText = target;
+        }
+      };
+      updateCount();
+    });
+
+    // mouse-hover for button
+    const $button = $('.submit');
+    $button.on('mouseover', function() {
+        $(this).css('background-color', 'green');
+    });
+    $button.on('mouseout', function() {
+        $(this).css('background-color', '');
+    });
+
+    // for testimonial carousel
+const multipleItemCarousel = document.querySelector("#testimonialCarousel");
+
+if (window.matchMedia("(min-width:576px)").matches) {
+  const carousel = new bootstrap.Carousel(multipleItemCarousel, {
+    interval: false,
+  });
+
+  var carouselWidth = $(".carousel-inner")[0].scrollWidth;
+  var cardWidth = $(".carousel-item").width();
+
+  var scrollPosition = 0;
+
+  $(".carousel-control-next").on("click", function () {
+    if (scrollPosition < carouselWidth - cardWidth * 3) {
+      console.log("next");
+      scrollPosition = scrollPosition + cardWidth;
+      $(".carousel-inner").animate({ scrollLeft: scrollPosition }, 800);
+    }
+  });
+  $(".carousel-control-prev").on("click", function () {
+    if (scrollPosition > 0) {
+      scrollPosition = scrollPosition - cardWidth;
+      $(".carousel-inner").animate({ scrollLeft: scrollPosition }, 800);
+    }
+  });
+} else {
+  $(multipleItemCarousel).addClass("slide");
+}
 })
-$('')
